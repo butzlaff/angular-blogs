27b663d6e192096ba8f7481c48174932a3ba1cad
butzlaff <emiliobutz@gmail.com>
2023-09-23 17:31:36 -0300

    feat: menu bar

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+interface MenuItem {
+  name: string;
+  icon: string;
+  route: string;
+}
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.css']
 })
 export class MenuBarComponent {
-
+  menuItems: MenuItem[] = [
+    {
+      name: 'Linkedin',
+      icon: 'linkedin',
+      route: 'https://www.linkedin.com/in/emilio-butzlaff/'
+    },
+    {
+      name: 'Github',
+      icon: 'github',
+      route: 'https://github.com/butzlaff'
+    },
+  ];
 }
